becf659bae2b3f9dd650463772544845fa75f933
Badar Saud Al-Qassabi <86981403+Bsaud@users.noreply.github.com>
2022-06-25 03:00:06 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ echo admin | sudo apt update
 sudo apt -y install samba
 sudo systemctl start smbd
 sudo \cp -r ~/sambaautoinstaller/s /etc/samba/smb.conf
-sudo \cp -r ~/sambaautoinstaller/x /etc/smbshared.conf
+sudo \cp ~/sambaautoinstaller/x /etc/smbshared.conf
 sudo groupadd smbgroup
 sudo useradd -g smbgroup smbuser
 sudo mkdir /share /share/Documents /share/Public
